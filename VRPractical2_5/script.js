fdddb21c9e9b0c2ae2f9459ff62a705f164c7c73
FreddyB22 <freddyb22@nycstudents.net>
2025-12-22 15:43:33 +0000

    fdgdfghd

--- VRPractical2_5/script.js
+++ VRPractical2_5/script.js
@@ -98,18 +98,12 @@ window.addEventListener("DOMContentLoaded",function() {
    Add appropriate classes to use as objects in your map.  Choose characters to represent these objects and position them on the map.   In Challenge 5 and 6, you will generate the map using the character representation of the objects you chose to place in the world. Get Creative!
 */
 
-let scene;
+
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   for(let r = 0; r < maze.length; r++){
-    /* Challenge 3
-      Choose a technique to traverse the each character in the string.
-    */ 
-    /* Challenge 4
-       Make an appropriate decision based on the characters you chose to enter 
-       in the maze.  Create an instance of the corresponding object.
-    */
+   
   }
 
 })
